bc54bd0332e18fd8eac8be735e6b7a28baf44f42
maricotagc <36520845+maricotagc@users.noreply.github.com>
2020-03-19 14:49:21 +0100

    Update TreeSet.java

--- Codes/TreeSet.java
+++ Codes/TreeSet.java
@@ -1,1 +1,43 @@
+package org.mariana.collectionsExamples.Set;
+import java.util.TreeSet;
 
+public class TreeSetTest {
+    public static void main(String[] args) {
+        //Declaration
+        TreeSet<String> treeSetName = new TreeSet<>();
+        System.out.println("After declaration");
+        for (String h : treeSetName) {
+            System.out.println(h);
+        }
+
+        //Addition
+        treeSetName.add("1");
+        treeSetName.add("0");
+        treeSetName.add("5");
+        treeSetName.add("3");
+        treeSetName.add("2");
+
+        System.out.println();
+        System.out.println("After addition");
+        for (String h : treeSetName) {
+            System.out.println(h);
+        }
+
+        //Deletion
+        treeSetName.remove("2");
+        System.out.println();
+        System.out.println("After deletion of  string '2'");
+        for (String h : treeSetName) {
+            System.out.println(h);
+        }
+
+        //Duplication
+        treeSetName.add("1");
+        treeSetName.add("1");
+        System.out.println();
+        System.out.println("After duplication of string '1'");
+        for (String h : treeSetName) {
+            System.out.println(h);
+        }
+    }
+}
