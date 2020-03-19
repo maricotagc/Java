4d7583472bfd59ee009d86ea3fc4313445f14969
maricotagc <36520845+maricotagc@users.noreply.github.com>
2020-03-19 14:55:57 +0100

    Update HashMap.java

--- Codes/HashMap.java
+++ Codes/HashMap.java
@@ -1,1 +1,49 @@
+package org.mariana.collectionsExamples.Map;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        //Declaration
+        HashMap<Integer, String> hashMapName = new HashMap<>();
+        System.out.println("After declaration:");
+        for (Map.Entry<Integer, String> entry : hashMapName.entrySet()) {
+            int key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println("Key: " + key + " ; Value: " + value);
+        }
+
+        //Addition
+        hashMapName.put(0, "Zero");
+        hashMapName.put(5, "Cinco");
+        hashMapName.put(1, "Um");
+        hashMapName.put(4, "Quatro");
+        hashMapName.put(2, "Dois");
+        hashMapName.put(3, "Tres");
+        System.out.println();
+        System.out.println("After Addition:");
+        for (Map.Entry<Integer, String> entry : hashMapName.entrySet()) {
+            int key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println("Key: " + key + " ; Value: " + value);
+        }
+
+        //Retrieval
+        System.out.println();
+        System.out.println("Result of .get(5):\n" + hashMapName.get(5));
+        System.out.println();
+        System.out.println("Result of .entrySet():\n" + hashMapName.entrySet());
+
+        //Deletion
+        hashMapName.remove(4);
+        System.out.println();
+        System.out.println("After Deletion of key = 4:");
+        for (Map.Entry<Integer, String> entry : hashMapName.entrySet()) {
+            int key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println("Key: " + key + " ; Value: " + value);
+        }
+    }
+}
