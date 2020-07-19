38fb044af0d1cde08d17469a31c57cef85e4b9cf
maricotagc <36520845+maricotagc@users.noreply.github.com>
2020-07-19 09:01:10 +0200

    Create ScannerUser.java

---
+++ Codes/ScannerUser.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.*;
+
+class ScannerFromUserInput {
+    public static void main(String[] args) {
+
+        Scanner scanner1 = new Scanner(System.in);
+
+        String str1 = scanner1.next();
+        String str2 = scanner1.next();
+        String str3 = scanner1.next();
+
+        System.out.println(str3);
+        System.out.println(str2);
+        System.out.println(str1);
+    }
+}
