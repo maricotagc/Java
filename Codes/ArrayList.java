cddff8fadaeff84b086092d088ade2844aaa343f
maricotagc <36520845+maricotagc@users.noreply.github.com>
2023-08-14 22:15:55 +0200

    Update ArrayList.java

--- Codes/ArrayList.java
+++ Codes/ArrayList.java
@@ -4,48 +4,101 @@ import java.util.ArrayList;
 public class ArrayListTest {
 
     public static void main(String[] args) {
-        //Declaration
-        ArrayList<String> arrayListName = new ArrayList<>();
+        	//ArrayList Declaration
+		List<String> arrayListName = new ArrayList<>();
 
-        System.out.println("After declaration");
-        for (int i = 0; i <= arrayListName.size() - 1; i++) {
-            System.out.println(arrayListName.get(i));
-        }
+		System.out.println("Before addition");
+		for (String item : arrayListName)
+		{
+			System.out.println(item);
+		}
 
-        //Addition
-        arrayListName.add("Number 2");
-        arrayListName.add("Number 1");
-        arrayListName.add("Number 0");
-        arrayListName.add("Number 3");
+		//Addition
+		arrayListName.add("Number 2");
+		arrayListName.add("Number 1");
+		arrayListName.add("Number 0");
+		arrayListName.add("Number 3");
 
-        System.out.println("After addition");
-        for (int i = 0; i <= arrayListName.size() - 1; i++) {
-            System.out.println(arrayListName.get(i));
-        }
+		System.out.println(" ");
+		System.out.println("After addition");
+		for (String item : arrayListName)
+		{
+			System.out.println(item);
+		}
 
-        //Retrieval
-        System.out.println();
-        System.out.println("Retrieval --> .get(0) = " + arrayListName.get(0));
+		//Get a value
+		System.out.println();
+		System.out.println(".get(0) = " + arrayListName.get(0));
 
-        //Deletion
-        arrayListName.remove(0);
-        System.out.println("After deletion of i=0");
-        for (int i = 0; i <= arrayListName.size() - 1; i++) {
-            System.out.println(arrayListName.get(i));
-        }
+		//Deletion
+		arrayListName.remove(0);
+		System.out.println(" ");
+		System.out.println("After deletion of index 0");
+		for (String item : arrayListName)
+		{
+			System.out.println(item);
+		}
 
-        //Duplication
-        arrayListName.add(2, "Duplicated Index");
-        System.out.println("After duplication of i=2");
-        for (int i = 0; i <= arrayListName.size() - 1; i++) {
-            System.out.println(arrayListName.get(i));
-        }
+		//Duplication
+		arrayListName.add(2, "Duplicated Index");
+		System.out.println(" ");
+		System.out.println("After duplication of i = 2");
+		for (String item : arrayListName)
+		{
+			System.out.println(item);
+		}
 
-        //Null
-        arrayListName.add(0,null);
-        System.out.println("After .add(0, null)");
-        for (int i = 0; i <= arrayListName.size() - 1; i++) {
-            System.out.println(arrayListName.get(i));
-        }
-    }
+		//Null
+		arrayListName.add(4,null);
+		System.out.println(" ");
+		System.out.println("After .add(4, null)");
+		for (String item : arrayListName)
+		{
+			System.out.println(item);
+		}
+
+		//Using toArray()
+		Object[] newArray = arrayListName.toArray();
+		System.out.println(" ");
+		System.out.println("Transforming ArrayList in Array");
+		for (Object item : newArray)
+		{
+			System.out.println(item);
+		}
+
+		//Removing Null
+		arrayListName.remove(4);
+		System.out.println(" ");
+		System.out.println("After removing null");
+		for (String item : arrayListName)
+		{
+			System.out.println(item);
+		}
+
+		//Sorting a List
+		System.out.println(" ");
+		System.out.println("Unsorted arrayListName");
+		for (String item : arrayListName)
+		{
+			System.out.println(item);
+		}
+
+
+		Collections.sort(arrayListName);
+		System.out.println(" ");
+		System.out.println("Sorted arrayListName");
+		for (String item : arrayListName)
+		{
+			System.out.println(item);
+		}
+
+		//Using subList()
+		List<String> subList = arrayListName.subList(1, 3);
+		System.out.println(" ");
+		System.out.println("Using subList()");
+		for (String item : subList)
+		{
+			System.out.println(item);
+		}
+	}
 }
