c574ee4bf0d920c34908a009a980f6a25bdc8c8c
maricotagc <36520845+maricotagc@users.noreply.github.com>
2023-08-21 17:16:27 +0200

    Update HashSet.java

--- Codes/HashSet.java
+++ Codes/HashSet.java
@@ -4,51 +4,70 @@ import java.util.HashSet;
 
 public class HashSetTest {
     public static void main(String[] args) {
-        //Declaration
-        HashSet<String> hashSetName = new HashSet<>();
-        System.out.println("After declaration");
-        for (String h : hashSetName) {
-            System.out.println(h);
-        }
+        		//Set is composed of unique, unordered elements. It is part of Collections interface.
 
-        //Addition
-        hashSetName.add("1");
-        hashSetName.add("0");
-        hashSetName.add("5");
-        hashSetName.add("3");
-        hashSetName.add("2");
+		//Declaration
+		Set<String> hashset1 = new HashSet<>();
+		Set<String> hashset2 = new HashSet<>();
 
-        System.out.println();
-        System.out.println("After addition");
-        for (String h : hashSetName) {
-            System.out.println(h);
-        }
+		//Addition
+		hashset1.add("Zambia");
+		hashset1.add("Brasil");
+		hashset1.add("Alemanha");
+		hashset1.add("Equador");
+		hashset1.add("Dinamarca");
 
-        //Deletion
-        hashSetName.remove("2");
-        System.out.println();
-        System.out.println("After deletion of string '2'");
-        for (String h : hashSetName) {
-            System.out.println(h);
-        }
-        System.out.println();
+		System.out.println();
+		System.out.println("hashset1 after addition");
+		System.out.println(hashset1);
 
-        //Duplication
-        hashSetName.add("1");
-        hashSetName.add("1");
-        System.out.println();
-        System.out.println("After duplication of string '1'");
-        for (String h : hashSetName) {
-            System.out.println(h);
-        }
-        System.out.println();
+		hashset2.add("Malasia");
+		hashset2.add("Lituania");
+		hashset2.add("Equador");
+		hashset2.add("Alemanha");
+		hashset2.add("Oma");
 
-        //Null
-        hashSetName.add(null);
-        System.out.println();
-        System.out.println("After null");
-        for (String h : hashSetName) {
-            System.out.println(h);
-        }
+		System.out.println();
+		System.out.println("hashset2 after addition");
+		System.out.println(hashset2);
+
+		//Duplication
+		hashset1.add("Alemanha");
+		System.out.println();
+		System.out.println("hashset1 after duplication of string 'Alemanha'");
+		System.out.println(hashset1);
+		//Duplication
+		hashset2.add("Malasia");
+		System.out.println();
+		System.out.println("hashset2 after duplication of string 'Malasia'");
+		System.out.println(hashset2);
+
+		//Intersection
+		Set<String> intersection = new HashSet<>(hashset1);
+		intersection.retainAll(hashset2);
+		System.out.println(" ");
+		System.out.println("Intersection: " + intersection);
+
+		//Union
+		Set<String> union = new HashSet<>(hashset1);
+		union.addAll(hashset2);
+		System.out.println(" ");
+		System.out.println("Union: " + union);
+
+		//Null addition
+		union.add(null);
+		System.out.println();
+		System.out.println("Union after null addition: " + union);
+
+		//Deletion
+		union.remove(null);
+		System.out.println();
+		System.out.println("Union after deletion of null: " + union);
+
+		//Sorting has to be through TreeSet
+		Set<String> unionTreeSet = new TreeSet<>(union);
+		System.out.println(" ");
+		System.out.println("After transforming to TreeSet the elements are automatically sorted: " + unionTreeSet);
+	}
     }
 }
